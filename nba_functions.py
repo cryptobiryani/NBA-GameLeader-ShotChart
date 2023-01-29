e2d1183133b155030e77b54cbdffdbaf48b7d3bf
cryptobiryani <realcryptobiryani@gmail.com>
2023-01-29 10:55:48 -0500

    updated get_team_code_function, fixed game info bug

--- nba_functions.py
+++ nba_functions.py
@@ -30,7 +30,7 @@ def get_game_code(game_dict):
 def get_game_id(game_dict):
     game_id_list=[]
     for game in game_dict:
-        if game['gameStatusText'] == 'Final':
+        if 'Final' in game['gameStatusText'] :
             game_id_list.append(game['gameId'])
     return game_id_list
 def get_box_score(game_id):
@@ -47,10 +47,10 @@ def get_scorer_df(games_dict, game_code, is_home=True):
         leader_index='awayLeaders'
         team_index='awayTeam'
     for game in games_dict:
-        if game['gameStatusText'] == 'Final' and game['gameCode'] == game_code:
+        if 'Final' in game['gameStatusText'] and game['gameCode'] == game_code:
             scoring_leader=game['gameLeaders'][leader_index]['personId']
             team=game[team_index]['teamId']
-            data=shotchartdetail.ShotChartDetail(player_id=scoring_leader, last_n_games=1, team_id=team).get_dict()['resultSets'][0]
+            data=shotchartdetail.ShotChartDetail(player_id=scoring_leader, last_n_games=1, team_id=team, context_measure_simple = 'PTS').get_dict()['resultSets'][0]
             rows=data['rowSet']
             headers=data['headers']
             df=pd.DataFrame(rows)
@@ -74,18 +74,20 @@ def get_player_name(scorer_df):
     '''
     return scorer_df['PLAYER_NAME'][0]
 
-def get_team_acroynm(scorer_df, is_home=True):
+def get_team_acroynm(game_code, is_home=True):
     '''
     Params: Dataframe of scorer containing HTM and VTM columns, boolean indicating whether you want the home team or away team's acronym
     Returns: String containing the team's acronym
     '''
     if is_home == True:
-        return scorer_df['HTM'][0]
+            return game_code[9:12]
+    #     return scorer_df['HTM'][0]
     elif is_home == False:
-        return scorer_df['VTM'][0]
-    elif is_home not in (True, False):
-        print("is_home must be either True or False.")
-        return None
+        return game_code[12:15]
+    #     return scorer_df['VTM'][0]
+    # elif is_home not in (True, False):
+    #     print("is_home must be either True or False.")
+    #     return None
 def get_game_date(game_code):
     '''
     params: game code in this format --> 20230113/HOUSAC
@@ -94,5 +96,4 @@ def get_game_date(game_code):
     game_date_yyyymmdd=game_code.split('/')[0]
     game_date = datetime.strptime(game_date_yyyymmdd, '%Y%m%d').strftime('%m/%d/%Y')
     return game_date
-
 
